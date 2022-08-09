ac4584e5feca39f92a73c44423e0a7032667b750
Luan Roberto Estrada Martins <luanrobertoestradamartins@MacBook-Air-de-Luan.local>
2022-08-08 21:56:15 -0300

    linkedList started

---
+++ Top-Interview-Questions/linkedList/1-Delete-Node-in-a-Linked-List/linkedlist.js
@@ -0,0 +1,88 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val)
+        
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+           this.tail.next = newNode
+           this.tail = newNode 
+         }
+         this.length++
+         return this
+       }
+
+    pop(){
+        if(!this.head) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        var previousNode = this.get(index - 1);
+        var removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+}
+
+let list = new SinglyLinkedList()
+
+for(let i = 1; i<5; i++)
+{
+    const newNode = new Node(i)
+    console.log(list)
+    list.push(newNode)
+}
+console.log(list)
